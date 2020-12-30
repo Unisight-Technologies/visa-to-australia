4b8375c21015786e7b65fd3eaec7243c0356c468
Prajol Sethi <prajolsethi@gmai.com>
2020-12-30 18:45:54 +0530

    why us

--- australia_app/models.py
+++ australia_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# contact pages
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    subject = models.CharField(max_length=400)
+    message = models.CharField(max_length=800)
--- australia_app/views.py
+++ australia_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View, TemplateView
-
+from . import models
+from . import mailHandler
+# import requests
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 class Homepage(TemplateView):
@@ -11,6 +15,29 @@ class Aboutpage(TemplateView):
 
 class Contactpage(TemplateView):
     template_name= "contactus.html"
+
+    def post(self, request):
+
+        form = request.POST
+        name = form.get('name')
+        email = form.get('email')
+        phone = form.get('phone')
+        subject = form.get('subject')
+        message = form.get('message')
+
+        new_contact = models.Contact.objects.create(
+            name=name,
+            email=email,
+            phone=phone,
+            subject=subject,
+            message=message
+
+        )
+        new_contact.save()
+        mailHandler.sendMailToUser(name, email)
+        mailHandler.sendMailToVisaToCanada(name, email, phone, subject, message)
+        messages.success(request, "Your query has been successfully submitted. We will get back to you soon.")
+        return redirect("contactus")
 class Workpage(TemplateView):
     template_name= "workvisa.html"
 class Studentpage(TemplateView):
@@ -18,4 +45,4 @@ class Studentpage(TemplateView):
 class Investorpage(TemplateView):
     template_name= "investorvisa.html"
 class Goldenpage(TemplateView):
-    template_name= "goldenvisa.html"    
+    template_name= "goldenvisa.html"
