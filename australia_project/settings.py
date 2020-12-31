c81fafbb49aa9cdda3b635deae75958bf38f2a2f
Ananya Nigotia <51746770+nigotia30@users.noreply.github.com>
2020-12-31 15:28:05 +0530

    static file updated

--- australia_project/settings.py
+++ australia_project/settings.py
@@ -132,4 +132,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 import os.path
 STATIC_URL = '/static/'
-STATIC_ROOT=(os.path.join(BASE_DIR,'staticfiles'),)
+STATICFILES_DIRS=(os.path.join('static'),)
+STATIC_ROOT= '/static/'
